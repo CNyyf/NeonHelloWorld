79d09bc7bfe0703cb0e14be840b4f6c575921a19
CNyyf <cn_yyf@163.com>
2016-11-01 13:13:51 +0800

    testChange201611011313a

--- src/helloworld/Person.java
+++ src/helloworld/Person.java
@@ -4,15 +4,21 @@ public class Person {
 	private int age;
 	private String name;
 	public int getAge() {
+		age++;
 		return age;
 	}
 	public void setAge(int age) {
 		this.age = age;
+		age++;
 	}
 	public String getName() {
+		if(name!=null)
+			name+="/";
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+		if(name!=null)
+			name+="/";
 	}
 }
